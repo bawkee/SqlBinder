14e3060d874a957d7f6262ef1af3621d8a2e00bd
Bojan Sala <bojan.sala@yahoo.com>
2018-05-21 19:36:58 +0200

    Updated demo.

--- Source/SqlBinder.DemoApp/App.xaml.cs
+++ Source/SqlBinder.DemoApp/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using SqlBinder.DemoNorthwindDal;
 using SqlBinder.DemoNorthwindDal.OleDb;
 
