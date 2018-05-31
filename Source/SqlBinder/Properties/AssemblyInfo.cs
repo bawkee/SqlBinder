066ac927a8373437a7351d9281ffd1cf55a7d6cb
Bojan Sala <bojan.sala@yahoo.com>
2018-05-31 16:51:15 +0200

    Nuget packer implemented.

--- Source/SqlBinder/Properties/AssemblyInfo.cs
+++ Source/SqlBinder/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bojan Sala")]
 [assembly: AssemblyProduct("SqlBinder")]
-[assembly: AssemblyCopyright("Copyright © Bojan Sala 2017")]
+[assembly: AssemblyCopyright("Copyright © Bojan Sala")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
